252fc45dfd24b03a1084f6ff0cc81863469a1772
raptor4444 <nishantgiri19722000@gmail.com>
2022-09-30 22:43:11 +0530

    jsx practice

---
+++ 01 - JSX/script.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render( 
+  <div>
+    <h1>
+      How to Greet
+    </h1>
+    <ul>
+      <li>Namaste</li>
+      <li>Hello</li>
+      <li>Hola</li>
+    </ul> 
+  </div>
+  ,document.getElementById("root"));
